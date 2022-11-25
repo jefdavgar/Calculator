d6008800e11138c9a8c3fb24433a848ad0514d3a
Jeferson Camero <jeferson.camero@cun.edu.co>
2022-11-25 10:03:58 -0500

    Este es el primer commit para la calculadora, se agrego los ficheros de HTML y CSS

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+const clean_one = document.getElementById("btn-borrar");
+const input = document.querySelector("input");
+const number9 = document.getElementById("number")
+const form = document.querySelector("form")
+text = number9.textContent
+const p = document.createElement("p")
+form.appendChild(p)
+p.textContent = text
